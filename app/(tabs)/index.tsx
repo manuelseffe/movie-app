d4b5a1475b4a5715f9e25f78794941b3c482405e
Manuels Effe <manuelseffe@gmail.com>
2025-10-20 19:13:20 +0100

    working on the tabs

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
